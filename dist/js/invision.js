d7e9b46a94a642ad46321088b2dfea4e11b3544e
Bevis <bevis724@users.noreply.github.com>
2020-03-03 00:51:38 +0800

    Update invision.js

--- dist/js/invision.js
+++ dist/js/invision.js
@@ -1,8 +1,13 @@
 'use strict';
 
 $(document).ready(function () {
-  $('.header__menu').click(function () {
+  $('.header__menu').on('click', function () {
     $('.header__rwdmenu').toggleClass('header__rwdmenu--open');
     $('.header__menu').toggleClass('header__menu--open');
   });
 });
+// $(document).on("click", function (e) {
+//   if ($(e.target).parents(".header__rwdmenu").length == 0) {
+//     $(".header__rwdmenu").hide();
+//   }
+// });
